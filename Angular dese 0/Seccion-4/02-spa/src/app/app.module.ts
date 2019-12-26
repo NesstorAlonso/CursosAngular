f3da6de103cf37a09f58092420cdda9bf58cc342
Netor Alonso <nexxt_alucar.com>
2019-12-26 17:44:16 -0600

    clase 57 seccion 4

--- Angular dese 0/Seccion-4/02-spa/src/app/app.module.ts
+++ Angular dese 0/Seccion-4/02-spa/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 // Rutas 
 import { APP_ROUTING } from './app.routes'
 // Servicios 
+import { HeroesService } from './servicios/heroes.service'; 
 
 //Componentes
 import { AppComponent } from './app.component';
@@ -13,7 +14,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
-¿
+import { HeroeComponent } from './components/heroe/heroe.component';
+
 
 
 @NgModule({
@@ -22,14 +24,17 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
