99e2373c661e9fff3bf7bfb22e08608a113d393a
Netor Alonso <nexxt_alucar.com>
2020-01-15 18:12:06 -0600

    Angular desde 0 sec 10 cap 178

--- Angular dese 0/Seccion-10/angular-login-base-1.0.0/src/app/pages/login/login.component.ts
+++ Angular dese 0/Seccion-10/angular-login-base-1.0.0/src/app/pages/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UsuarioModel } from '../../models/usuario.model';
+import { AuthService } from '../../services/auth.service';
+import  Swal  from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +13,46 @@ import { UsuarioModel } from '../../models/usuario.model';
 export class LoginComponent implements OnInit {
 
   usuario: UsuarioModel = new UsuarioModel();
-  constructor() { }
+  recordame = false;
+  constructor( private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
+
+    if( localStorage.getItem('email')){
+      this.usuario.email = localStorage.getItem('email');
+      this.recordame = true;
+    }
   }
+
   login(form:NgForm){
     if( form.invalid){ return;}
-    console.log(this.usuario);
-      console.log(form);
+
+    Swal.fire({
+      allowOutsideClick: false,
+      type: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
+  
+     this.auth.login(this.usuario)
+       .subscribe(resp  => { console.log(resp);
+        Swal.close();
+
+        if(this.recordame){
+          localStorage.setItem('email', this.usuario.email);
+        }
+
+        this.router.navigateByUrl('/home');
+      }, (err) =>{
+        console.log(err.error.error.message);
+        Swal.fire({
+          type: 'error',
+          title: 'Error al autenticar',
+          text: err.error.error.message
+        });
+      });
+     
   }
 
 }
--- Angular dese 0/Seccion-10/angular-login-base-1.0.0/src/app/pages/registro/registro.component.ts
+++ Angular dese 0/Seccion-10/angular-login-base-1.0.0/src/app/pages/registro/registro.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from '../../models/usuario.model';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import  Swal  from 'sweetalert2';
+import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -11,8 +15,9 @@ import { NgForm } from '@angular/forms';
 export class RegistroComponent implements OnInit {
 
 usuario: UsuarioModel;
+recordarme = false;
 
-  constructor() { }
+  constructor( private auth: AuthService, private router: Router) { }
 
   ngOnInit() { 
 
@@ -26,9 +31,30 @@ usuario: UsuarioModel;
     {
       return;
     }
-    console.log('Enviado');
-    console.log(this.usuario);
-    console.log(form);
+    
+    Swal.fire({
+      allowOutsideClick: false,
+      type: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
+    this.auth.nuevoUsuario(this.usuario)
+    .subscribe( resp => {
+      Swal.close();
+    if(this.recordarme){
+      localStorage.setItem('email', this.usuario.email);
+    }
+
+      this.router.navigateByUrl('/home');
+    }, (err) =>{
+      console.log(err.error.error.message);
+      Swal.fire({
+        type: 'error',
+        title: 'Error al autenticar',
+        text: err.error.error.message
+      });
+    });
   }
 
 
